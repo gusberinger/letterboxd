3c6cc6b996bbfe8ed2838b546066768a469e5b34
Gus Beringer <berin013@umn.edu>
2022-03-31 14:22:58 -0500

    added parser

--- src/download.py
+++ src/download.py
@@ -1,13 +1,14 @@
-from ast import parse
+import argparse
 import itertools
 import re
+from unicodedata import numeric
 import requests
 from bs4 import BeautifulSoup
 
 base_url = "https://letterboxd.com"
 imdb_pattern = re.compile(r"http:\/\/www\.imdb\.com/title/(tt\d{7,8})/maindetails")
 
-def _find_links_in_list(list_link):
+def _find_links_in_list(list_link, limit = float("inf"), acc = 0):
     """Finds all the links from a list"""
     response = requests.get(list_link)
     page = response.text
@@ -16,9 +17,9 @@ def _find_links_in_list(list_link):
     items = table.find_all("li")
     movie_links = (f"{base_url}{li.div.get('data-film-slug')}" for li in items)
     yield from movie_links
-    if next_url_tag := soup.find("a", class_="next"):
+    if next_url_tag := soup.find("a", class_="next") and acc < limit:
         next_url = f"{base_url}{next_url_tag.get('href')}"
-        yield from _find_links_in_list(next_url)
+        yield from _find_links_in_list(next_url, limit, acc + len(items))
 
 
 def _parse_link(movie_link):
@@ -27,14 +28,26 @@ def _parse_link(movie_link):
     soup = BeautifulSoup(page, "html.parser")
     imdb_tag = soup.find("a", {"data-track-action": "IMDb"})
     imdb_url = imdb_tag.get("href")
-    print(imdb_url)
     imdb_id = re.match(imdb_pattern, imdb_url).group(1)
     return imdb_id
 
 
 def download_list(list_link, limit=None):
-    movie_links = _find_links_in_list(list_link)
-    return itertools.islice(movie_links, limit)
+    if limit is None:
+        numerical_limit = float("inf")
+    else:
+        numerical_limit = limit
+
+    movie_links = _find_links_in_list(list_link, limit=numerical_limit)
+    imdb_ids = (_parse_link(movie) for movie in movie_links)
+    return itertools.islice(imdb_ids, limit)
 
 if __name__ == "__main__":
-    print(list(download_list("https://letterboxd.com/hackfraud/likes/films/by/member-rating/")))
+    parser = argparse.ArgumentParser(description='Process letterbox link')
+    parser.add_argument('url', metavar='url', type=str, help="The complete url to the letterboxd list")
+    parser.add_argument('-limit', dest="limit", type=int, default=None)
+    args = vars(parser.parse_args())
+    print(args)
+    print(list(download_list(args['url'], limit = args['limit'])))
+
+    # print(list(download_list("https://letterboxd.com/hackfraud/likes/films/by/member-rating/", limit=50)))

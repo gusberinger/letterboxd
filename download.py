a9a31866d8f17a4a6d01f8ada0059578a348e774
Gus Beringer <berin013@umn.edu>
2022-03-31 13:26:41 -0500

    renamed variables

--- download.py
+++ download.py
@@ -1,3 +1,4 @@
+from ast import parse
 import re
 import requests
 from bs4 import BeautifulSoup
@@ -6,22 +7,22 @@ base_url = "https://letterboxd.com"
 
 imdb_pattern = re.compile(r"http:\/\/www\.imdb\.com/title/(tt\d{7})/maindetails")
 
-def find_links_in_list(link):
+def _find_links_in_list(list_link):
     """Finds all the links from a list"""
-    response = requests.get(link)
+    response = requests.get(list_link)
     page = response.text
     soup = BeautifulSoup(page, "html.parser")
     table = soup.find("ul", class_="poster-list")
     items = table.find_all("li")
-    links = (f"{base_url}{li.div.get('data-film-slug')}" for li in items)
-    yield from links
+    movie_links = (f"{base_url}{li.div.get('data-film-slug')}" for li in items)
+    yield from movie_links
     if next_url_tag := soup.find("a", class_="next"):
         next_url = f"{base_url}{next_url_tag.get('href')}"
-        yield from find_links_in_list(next_url)
+        yield from _find_links_in_list(next_url)
 
 
-def parse_link(link):
-    response = requests.get(link)
+def _parse_link(movie_link):
+    response = requests.get(movie_link)
     page = response.text
     soup = BeautifulSoup(page, "html.parser")
     imdb_tag = soup.find("a", {"data-track-action": "IMDb"})
@@ -29,8 +30,13 @@ def parse_link(link):
     imdb_id = re.match(imdb_pattern, imdb_url).group(1)
     return imdb_id
 
+
+def download_list(list_link):
+    movie_links = _find_links_in_list(list_link)
+    return (_parse_link(movie_id) for movie_id in movie_links)
+
     
 
 
 if __name__ == "__main__":
-    for links in tqdm(find_links_in_list())
+    print(list(download_list("https://letterboxd.com/testuser_py/likes/films/")))

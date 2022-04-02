37d2bb61802e47f63ca95742e6e9548c4d399324
Gus Beringer <gusberinger@gmail.com>
2022-04-02 21:19:46 +0100

    removed parse from main

--- src/letterboxd_convert/__main__.py
+++ src/letterboxd_convert/__main__.py
@@ -1,15 +1,6 @@
 from .download import download_list
-import argparse
+from .parse import parse_args
 import sys
-
-def parse_args(args):
-    parser = argparse.ArgumentParser(description='Process letterbox link')
-    parser.add_argument('url', metavar='url', type=str, help="The complete url to the letterboxd list")
-    parser.add_argument('-limit', '-l', dest="limit", type=int, default=None)
-    result = vars(parser.parse_args(args))
-    if not result['url'].startswith('https://letterboxd.com/'):
-        raise ValueError("Not a valid url.")
-    return result
 
 
 def main():

f0d91d7b87ff814032a67d03a4a704cdcf69b9c1
Gus Beringer <gusberinger@gmail.com>
2022-04-02 21:13:39 +0100

    relaxed url requirements

---
+++ src/letterboxd_convert/parse.py
@@ -0,0 +1,16 @@
+import argparse
+import re
+
+url_match = re.compile(r"(?:https://)?(?:www\.)?(letterboxd\.com/.*)")
+
+
+def parse_args(args):
+    parser = argparse.ArgumentParser(description='Process letterbox link')
+    parser.add_argument('url', metavar='url', type=str, help="The complete url to the letterboxd list")
+    parser.add_argument('-limit', '-l', dest="limit", type=int, default=None)
+    result = vars(parser.parse_args(args))
+    url_body = re.match(url_match, result['url'])
+    if url_body is None:
+        raise ValueError("Not a valid letterboxd.com url")
+    result['url'] = f"https://{url_body.group(1)}"
+    return result

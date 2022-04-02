0f1cb44c733396fbaba5a48cd5c6681a75404474
Gus Beringer <gusberinger@gmail.com>
2022-04-02 20:10:14 +0100

    created progress bar

--- src/letterboxd_convert/download.py
+++ src/letterboxd_convert/download.py
@@ -1,16 +1,21 @@
 import argparse
 from functools import cache
 import itertools
+import math
 import re
 import sys
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 base_url = "https://letterboxd.com"
 imdb_pattern = re.compile(r"http:\/\/www\.imdb\.com/title/(tt\d{7,8})/maindetails")
 
-def _find_links_in_list(list_link, limit = float("inf"), acc = 0):
+def _find_links_in_list(list_link, limit = float("inf"), acc = 0, pbar = None):
     """Finds all the links from a list"""
+    if pbar is None:
+        pbar = tqdm(total = math.ceil(limit / 72))
+
     response = requests.get(list_link)
     page = response.text
     soup = BeautifulSoup(page, "html.parser")
@@ -18,10 +23,13 @@ def _find_links_in_list(list_link, limit = float("inf"), acc = 0):
     items = table.find_all("li")
     movie_links = (f"{base_url}{li.div.get('data-film-slug')}" for li in items)
     yield from movie_links
+    pbar.update(1)
     next_url_tag = soup.find("a", class_="next") 
     if next_url_tag and acc < limit:
         next_url = f"{base_url}{next_url_tag.get('href')}"
-        yield from _find_links_in_list(next_url, limit, acc + len(items))
+        yield from _find_links_in_list(next_url, limit, acc + len(items), pbar)
+    else:
+        pbar.close()
 
 
 @cache

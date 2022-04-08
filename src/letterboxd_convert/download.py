b0891daaf79beb9281aaa6b7aa50a4f2c9c100ed
Gus Beringer <berin013@umn.edu>
2022-04-08 03:58:54 -0500

    Revert "Merge branch 'master' of https://github.com/gusberinger/letterboxd into master"
    This reverts commit 3c6badf76ab6fdc700ca98cc6662d69ecfbd5c08, reversing
    changes made to 875bccc47d9205556c39822e71a8697aa572d03d.

--- src/letterboxd_convert/download.py
+++ src/letterboxd_convert/download.py
@@ -3,57 +3,45 @@ import itertools
 import re
 import time
 from typing import Iterable, Optional
-import httpx
-import asyncio
-from bs4 import BeautifulSoup, Tag
+import requests
+from bs4 import BeautifulSoup
 
 base_url = "https://letterboxd.com"
-imdb_pattern = re.compile(r"http:\/\/www\.imdb\.com/title/(tt\d{7,8})/maindetails")
+imdb_pattern = re.compile(
+    r"http:\/\/www\.imdb\.com/title/(tt\d{7,8})/maindetails"
+)
 
 
-def _find_pages_in_list(
-    list_url: str, limit: float = float("inf"), acc: int = 0, rate: float = 1
-) -> Iterable[str]:
+def _find_links_in_list(list_link: str, limit: float = float("inf"),
+                        acc: int = 0, rate: float = 1) -> Iterable[str]:
     """Finds all the links from a list"""
-    response = httpx.get(list_url)
+    response = requests.get(list_link)
     soup = BeautifulSoup(response.text, "html.parser")
-    poster_table = soup.find("ul", class_="poster-list")
-    assert isinstance(poster_table, Tag)
-    items = poster_table.find_all("li")
+    items = soup.find("ul", class_="poster-list").find_all("li")
     movie_links = (f"{base_url}{li.div.get('data-film-slug')}" for li in items)
     yield from movie_links
     next_url_tag = soup.find("a", class_="next")
     if next_url_tag and acc < limit:
-        assert isinstance(next_url_tag, Tag)
         next_url = f"{base_url}{next_url_tag.get('href')}"
         time.sleep(rate)
-        yield from _find_pages_in_list(next_url, limit, acc + len(items))
-
-
-async def download_pages(page_urls: Iterable[str]) -> Iterable[httpx.Response]:
-    async with httpx.AsyncClient() as client:
-        pages = (client.get(url) for url in page_urls)
-        responses = await asyncio.gather(*pages)
-    return responses
+        yield from _find_links_in_list(next_url, limit, acc + len(items))
 
 
 @cache
-def _parse_page(page_response: httpx.Response) -> str:
-    page = page_response.text
+def _parse_link(movie_link: str) -> str:
+    response = requests.get(movie_link)
+    page = response.text
     soup = BeautifulSoup(page, "html.parser")
     imdb_tag = soup.find("a", {"data-track-action": "IMDb"})
-    assert isinstance(imdb_tag, Tag)
     imdb_url = imdb_tag.get("href")
-    assert isinstance(imdb_url, str)
     imdb_id_match = re.match(imdb_pattern, imdb_url)
     assert imdb_id_match is not None
     imdb_id = imdb_id_match.group(1)
     return imdb_id
 
 
-def download_list(
-    list_url: str, limit: Optional[int] = None, rate: int = 1
-) -> Iterable[str]:
+def download_list(list_url: str,
+                  limit: Optional[int] = None, rate: int = 1) -> Iterable[str]:
     """
     Parameters
     ___
@@ -69,7 +57,9 @@ def download_list(
     else:
         numerical_limit = limit
     rate = max(rate, 1)
-    movie_links = _find_pages_in_list(list_url, limit=numerical_limit, rate=rate)
-    pages = asyncio.run(download_pages(movie_links))
-    imdb_ids = (_parse_page(page) for page in pages)
+    movie_links = _find_links_in_list(
+        list_url,
+        limit=numerical_limit,
+        rate=rate)
+    imdb_ids = (_parse_link(movie) for movie in movie_links)
     return itertools.islice(imdb_ids, limit)
